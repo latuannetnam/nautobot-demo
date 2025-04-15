a42d0270475fbc18818d8f8004ae6c8e19199e7a
Le Anh Tuan <latuannetnam@gmail.com>
2025-04-15 15:01:57 +0700

    delpoy configure from Nautobot to device

--- backup-config.sh
+++ backup-config.sh
@@ -1,2 +1,2 @@
  #ansible-inventory -i playbook/nautobot-inventory-01.yml --list 
- ansible-playbook -i playbook/nautobot-inventory-01.yml playbook/pb_backup_network.yml
+ ansible-playbook -vvvvv -i playbook/nautobot-inventory-01.yml playbook/pb_backup_network.yml
---
+++ config-devices.sh
@@ -0,0 +1,2 @@
+ #ansible-inventory -i playbook/nautobot-inventory-01.yml --list 
+ ansible-playbook -vvvvv -i playbook/nautobot-inventory-03.yml playbook/pb_deploy_ios_cfg.yml
