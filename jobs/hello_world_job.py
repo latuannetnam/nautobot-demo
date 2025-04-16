4939d514a4619ad49e5981e89ce3be5837535e13
Le Anh Tuan <latuannetnam@gmail.com>
2025-04-16 16:01:59 +0700

    test job v2

--- jobs/hello_world_job.py
+++ jobs/hello_world_job.py
@@ -4,6 +4,6 @@ class HelloWorld(Job):
     A simple job that prints "Hello, World!" to the console.
     """
     def run(self):
-        self.logger.debug("Hello, World!")
+        self.logger.debug("Hello, World! v2")
 
 register_jobs(HelloWorld)        
