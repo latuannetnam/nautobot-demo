9899a8095763fbee31d3dcf4550cc397b94ff6cb
Le Anh Tuan <latuannetnam@gmail.com>
2025-04-18 14:20:27 +0700

    test job hool v3

--- jobs/job_hook_test.py
+++ jobs/job_hook_test.py
@@ -7,7 +7,7 @@ class HelloWorldJobHook(JobHookReceiver):
     class Meta:
         name = "Hello World Job Hook"
 
-    def receive_job_hook(self, change, action):
-        self.logger.debug(f"Job hook received {change} {action}")
+    def receive_job_hook(self, change, action, changed_object):
+        self.logger.debug(f"Job hook received {change} {action} {changed_object}")
 
 register_jobs(HelloWorldJobHook)
